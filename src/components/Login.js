321666071ab7efbe7cb17bf5a6bfb44f20947b62
Ravi43chaurasiya <ravi8601150552@gmail.com>
2024-10-13 12:22:33 +0530

    header and login background

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div >
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/f272782d-cf96-4988-a675-6db2afd165e0/web/IN-en-20241008-TRIFECTA-perspective_b28b640f-cee0-426b-ac3a-7c000d3b41b7_large.jpg' alt='background image'></img>
+      </div>
+    </div>
+  )
+}
+
+export default Login
